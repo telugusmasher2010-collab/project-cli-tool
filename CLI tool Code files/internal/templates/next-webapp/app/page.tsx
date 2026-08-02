4836482e042e953b74d2e176ccd3caf0f3bb1879
Surender Reddy Enumula <surenderr@zenoti.com>
2026-08-02 19:24:29 +0530

    auto-sync Sun 08/02/2026 19:24:29.58

---
+++ CLI tool Code files/internal/templates/next-webapp/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1>Welcome to {{project_name}}</h1>
+      <p>
+        Get started by editing <code>app/page.tsx</code> and save to see your
+        changes.
+      </p>
+    </main>
+  );
+}
